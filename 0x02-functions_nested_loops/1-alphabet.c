620aa0d2d9be4b78fb6f09ab902cb94eae4dd7b1
Ogunsote Ayotunde <tobitoayo1@gmail.com>
2022-07-05 11:25:35 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,14 @@
-#include <main.h>
-#include <stdio.h>
+/*
+ *File: 1-alphabet.c
+ *
+ *Auth: Ayotunde Ogunsote
+ **/
+
+#include "main.h"
+
 /**
  *print_alphabet - prints the alphabet in lowercase, followed by a new line.
- */
+ **/
 void print_alphabet(void)
 {
 	char alpha;
