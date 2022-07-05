42c84dc2f203cf03c989f6d688b6de882c7d029f
Ogunsote Ayotunde <tobitoayo1@gmail.com>
2022-07-05 14:18:10 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,6 +12,7 @@ void times_table(void)
 
 	for (mult = 1; mult <= 9; mult++)
 	{
+
 	_putchar(',');
 	_putchar(' ');
 
@@ -20,6 +21,7 @@ void times_table(void)
 	if (prod <= 9)
 	_putchar(' ');
 	else
+
 	_putchar((prod / 10) + '0');
 
 	_putchar((prod % 10) + '0');
