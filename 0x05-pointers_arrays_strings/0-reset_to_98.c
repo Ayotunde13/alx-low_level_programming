d5ac7d41931918146a1136b98c58fb81a729ff89
Ogunsote Ayotunde <tobitoayo1@gmail.com>
2022-07-12 10:42:51 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,10 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * reset_to_98 - updates the value it points to to 98
  * @n: This is the entry
+ *
+ * Return: no return
  */
 
 void reset_to_98(int *n)
