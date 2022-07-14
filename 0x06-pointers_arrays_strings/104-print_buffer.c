410e16f98e9c2b3fc601e720b0f1f35ef1756903
Ogunsote Ayotunde <tobitoayo1@gmail.com>
2022-07-14 15:51:56 -0700

    Task 104

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,49 +1,50 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
- *print_buffer - Function
- *
- *Description:  prints a buffer.
- *print 10 bytes per line
- *line starts with position of the first byte of the line
- *in hexadecimal starting with 0
- *line shows the hexadecimal content
- *2 char of the buffer, 2bytes at a time
- *end with a new line
- *
- *@b: pointer parameter of type char,
- *@size: parameter of type int
- *
+ *print_buffer - Function prototype
+ *Description: Prints a buffer in hexadecimal and ASCII format
+ *@b: Pointer to the buffer
+ *@size: The size of the buffer
+ *Return: void
  */
 void print_buffer(char *b, int size)
 {
-	int byte, i;
+	int i;
+	int x;
+	int y;
 
-	for (byte = 0; byte < size; byte += 10)
-	{
-	printf("%08x: ", byte);
-	for (i = 0; i < 10; i++)
-	{
-	if ((i + byte) >= size)
-	printf("  ");
-	else
-	printf("%02x", *(b + i + byte));
-	if ((i % 2) != 0 && i != 0)
-	printf(" ");
-	}
-	for (i = 0; i < 10; i++)
-	{
-	if ((i + byte) >= size)
-	break;
-	else if (*(b + i + byte) >= 31 &&
-	*(b + i + byte) <= 126)
-	printf("%c", *(b + i + byte));
-	else
-	printf(".");
-	}
-	if (byte >= size)
-	continue;
-	printf("\n");
-	}
 	if (size <= 0)
+	{
 	printf("\n");
+	return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+	printf("%.8x:", i);
+
+	for (x = i; x < i + 10; x++)
+	{
+	if (x % 2 == 0)
+	printf(" ");
+	if (x < size)
+	printf("%.2x", *(b + x));
+	else
+	printf("  ")
+	}
+
+	printf(" ");
+
+	for (y = i; y < i + 10; y++)
+	{
+	if (y >= size)
+	break;
+
+	if (*(b + y) < 32 || *(b + y) > 126)
+	printf("%c", '.');
+	else
+	printf("%c", *(b + y));
+	}
+	printf("\n");
+	}
 }
