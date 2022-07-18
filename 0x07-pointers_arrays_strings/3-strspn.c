8c853a8b0a53b68c942a032a31e8885892d92fb7
Ogunsote Emmanuel Ayotunde <tobitoayo1@gmail.com>
2022-07-18 13:33:04 +0100

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,38 +1,29 @@
 #include "main.h"
 
 /**
- * _strspn - Locates a character in a string
- * @s: This is the main C string to be scanned.
- * @accept: This is the string containing the list of characters to match in s
- * Return: return count
+ * _strspn - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Return: bytes required to find accept
  */
-
 unsigned int _strspn(char *s, char *accept)
 {
-int i, j;
-int count = 0;
-char *str1, *str2;
+int i, j, exit;
 
-str1 = s;
-str2 = accept;
-i = 0;
-while (str1[i] != '\0')
+for (i = 0; s[i] != '\0'; i++)
 {
-j = 0;
-while (str2[j] != '\0')
+exit = 0;
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (str2[j] == str1[i])
+if (accept[j] == s[i])
 {
-count++;
-break;
+exit = 1;
 }
-j++;
 }
-if (s[i] != accept[j]);
+if (exit == 0)
 {
-break;
+return (i);
 }
-i++;
 }
-return (count);
+return (i);
 }
