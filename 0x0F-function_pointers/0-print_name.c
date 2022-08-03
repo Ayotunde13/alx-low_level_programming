25b13b56e6d42a760ac1b06230c73ae225ca11ec
Ogunsote Emmanuel Ayotunde <tobitoayo1@gmail.com>
2022-08-03 13:30:04 +0100

    all

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include "function_pointers.h"
 #include <stdlib.h>
 
 /**
@@ -12,5 +13,5 @@ void print_name(char *name, void (*f)(char *name))
 {
 if (name == NULL || f == NULL)
 return;
-f(name);
+(*f)(name);
 }
