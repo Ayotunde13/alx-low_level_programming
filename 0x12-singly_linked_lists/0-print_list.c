adb85da7ef284dee4d864a7d4a9b1d4bb952951a
Ogunsote Emmanuel Ayotunde <tobitoayo1@gmail.com>
2022-08-11 16:07:54 +0100

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,13 +1,13 @@
 #include "lists.h"
+
 /**
- * print_list - prints all the elements  a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
+* print_list - prints all the elements  a list_t list.
+* @h: singly linked list.
+* Return: number of elements in the list.
+*/
 size_t print_list(const list_t *h)
 {
 size_t nelem;
-
 nelem = 0;
 
 while (h != NULL)
