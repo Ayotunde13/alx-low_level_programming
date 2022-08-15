1bf2f2bfa7229cb4a4cbb8e6e24851e3ee536b2a
Ogunsote Emmanuel Ayotunde <tobitoayo1@gmail.com>
2022-08-15 22:20:00 +0100

    all tasks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,22 +3,20 @@
 #include <stddef.h>
 
 /**
- * print_listint - prints a listint with newlines between
- * each node's value
- *
- * @h: head of list to print
- *
- * Return: length of list
- */
+* print_listint - prints a listint with newlines between
+* each node's value
+*
+* @h: head of list to print
+*
+* Return: length of list
+*/
 size_t print_listint(const listint_t *h)
 {
-	size_t len;
+size_t len;
 
-	if (h == NULL)
-		return (0);
-
-	for (len = 0; h != NULL; h = h->next, len++)
-		printf("%d\n", h->n);
-
-	return (len);
+if (h == NULL)
+return (0);
+for (len = 0; h != NULL; h = h->next, len++)
+printf("%d\n", h->n);
+return (len);
 }
